6b527b656b8c48002dfdbbc836d4e98f5c55f97f
seniorazimi <seniorazimi@gmail.com>
2023-06-02 00:57:44 +0330

    Install django-rest-knox

--- minimal_django_rest/settings.py
+++ minimal_django_rest/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'  # to install rest_framework
+    'rest_framework',  # to install rest_framework
+    'knox'  # to install knox
 ]
 
 MIDDLEWARE = [
